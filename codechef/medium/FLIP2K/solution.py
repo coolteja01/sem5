32823ee595bc9d43957f3cbc33881b0a580709fc
Pavan teja <24b11ds008@adityauniversity.in>
2026-07-15 21:56:59 +0530

    ✅ FLIP2K | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-15

--- codechef/medium/FLIP2K/solution.py
+++ codechef/medium/FLIP2K/solution.py
@@ -1,34 +1,48 @@
-# cook your dish here
-import sys
-
-def solve():
-    input = sys.stdin.readline
-    t = int(input())
-    for _ in range(t):
-        n, k = map(int, input().split())
-        s = input().strip()
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    s = input().strip()
+    
+    zeros = s.count('0')
+    ones = n - zeros
+    
+    # If we can't perform even one operation
+    if zeros < k or ones < k:
+        print(s)
+        continue
+    
+    # Count zeros at each position modulo k
+    # This is the invariant that must be preserved
+    cnt = [0] * k
+    for i, ch in enumerate(s):
+        if ch == '0':
+            cnt[i % k] += 1
+    
+    # Build the lexicographically smallest string
+    ans = ['1'] * n
+    placed = 0
+    used = [0] * k
+    
+    # Place zeros as early as possible
+    for i in range(n):
+        if placed >= zeros:
+            break
         
-        zeros = s.count('0')
-        ones = n - zeros
-        
-        if zeros < k or ones < k:
-            print(s)
-            continue
-        cnt = [0] * k
-        for i, c in enumerate(s):
-            if c == '0':
-                cnt[i % k] += 1
-        ans = ['1'] * n
-        placed = 0
-        for i in range(n):
-            if placed < zeros:
-              
+        r = i % k
+        # We can place a zero here if we haven't used all
+        # the zeros that should be at positions with remainder r
+        if used[r] < cnt[r]:
+            ans[i] = '0'
+            used[r] += 1
+            placed += 1
+    
+    # Place remaining zeros at the end (if any)
+    if placed < zeros:
+        for i in range(n - 1, -1, -1):
+            if placed >= zeros:
+                break
+            if ans[i] == '1':
                 ans[i] = '0'
                 placed += 1
-            else:
-                break
-        
-        print(''.join(ans))
-
-if __name__ == "__main__":
-    solve()
+    
+    print(''.join(ans))
