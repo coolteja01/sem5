b6975359b0eadb486e7fba30333a679736a0d41a
Pavan teja <24b11ds008@adityauniversity.in>
2026-07-15 20:10:46 +0530

    ✅ ALTARR | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-15

--- codechef/medium/ALTARR/solution.py
+++ codechef/medium/ALTARR/solution.py
@@ -1,1 +1,22 @@
-def pa
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    changes1 = 0
+    for i in range(n):
+        if i % 2 == 0:  
+            if arr[i] % 2 == 0:
+                changes1 += 1
+        else: 
+            if arr[i] % 2 == 1:
+                changes1 += 1
+    changes2 = 0
+    for i in range(n):
+        if i % 2 == 0:  
+            if arr[i] % 2 == 1:
+                changes2 += 1
+        else: 
+            if arr[i] % 2 == 0:
+                changes2 += 1
+    
+    print(min(changes1, changes2))
