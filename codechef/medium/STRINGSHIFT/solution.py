83b790d6f6fb046ef80111a56bbf7daf5c50964c
Pavan teja <24b11ds008@adityauniversity.in>
2026-07-15 21:16:51 +0530

    ✅ STRINGSHIFT | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-15

--- codechef/medium/STRINGSHIFT/solution.py
+++ codechef/medium/STRINGSHIFT/solution.py
@@ -1,4 +1,20 @@
-t =int(input())
-for i in range t 
-    n=int(input())
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = list(input().strip())
+    start = -1
+    for i in range(n):
+        if s[i] != 'a':
+            start = i
+            break
     
+    if start == -1:
+        s[-1] = 'b'
+    else:
+        i = start
+        while i < n and s[i] != 'a':
+            s[i] = chr(ord(s[i]) + 1)
+            i += 1
+    
+    print(''.join(s))
