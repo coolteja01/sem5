7d4b9e0897f3ba0da9bb71d75fb560d41b24491c
Pavan teja <24b11ds008@adityauniversity.in>
2026-07-15 20:21:14 +0530

    ✅ CONVEYOR | Medium | Python | Runtime: N/A | Memory: N/A | 2026-07-15

--- codechef/medium/CONVEYOR/solution.py
+++ codechef/medium/CONVEYOR/solution.py
@@ -1,3 +1,13 @@
 t = int(input())
-for in range (t)
- n,
+for _ in range(t):
+    n, p = map(int, input().split())
+    s = input().strip()
+    for i in range(p):  
+        if s[i] == 'R':
+            left_reversals += 1
+    right_reversals = 0
+    for i in range(p - 1, n):  
+        if s[i] == 'L':
+            right_reversals += 1
+    
+    print(min(left_reversals, right_reversals))
